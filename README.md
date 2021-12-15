b0887f500e45140a247fa588b85fc2c2d29dca5d
DEVARAJSINGH <devarajsingh5@gmail.com>
2021-12-15 12:50:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-pqnhzm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-pqnhzm)
---
+++ src/Components/MobileUI/MoblieUI.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const MobileUI = ()=> {
+  return (
+    <div>
+      Hello Moile UI
+    </div>
+  )
+}
+export default MobileUI;
